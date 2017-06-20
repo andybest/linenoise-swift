251ac35725b65df0ba1713b861175ec8c10d121d
Andy Best <andybest.net@gmail.com>
2017-06-20 11:58:13 +0100

    Updated package manifest for Linux due to case sensitive filesystem

--- Package.swift
+++ Package.swift
@@ -23,7 +23,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "LineNoise",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources/linenoise"),
         .target(
             name: "linenoiseDemo",
             dependencies: ["LineNoise"]),
